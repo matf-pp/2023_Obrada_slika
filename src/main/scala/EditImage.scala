4c8e211f960a1168edd224df5776e6340a4c634f
Tijana Jakšić <127666916+TijanaJaksic@users.noreply.github.com>
2023-04-29 00:54:52 +0200

    Add files via upload

--- src/main/scala/EditImage.scala
+++ src/main/scala/EditImage.scala
@@ -53,19 +53,43 @@ class EditImage(val image: Image) {
     }
 
     def rotate(img: BufferedImage): BufferedImage = {
-    val w = img.getWidth
-    val h = img.getHeight
+        val w = img.getWidth
+        val h = img.getHeight
 
-    val out = new BufferedImage(h, w, BufferedImage.TYPE_INT_RGB)
+        val out = new BufferedImage(h, w, BufferedImage.TYPE_INT_RGB)
 
-    for (x <- 0 until w){
-        for (y <- 0 until h){
-            out.setRGB(h - y - 1, x, img.getRGB(x, y) & 0xffffff)
+        for (x <- 0 until w){
+            for (y <- 0 until h){
+                out.setRGB(h - y - 1, x, img.getRGB(x, y) & 0xffffff)
+            }
         }
+        out
     }
-    
-    
-    out
+
+    def brightness(b: Double, img: BufferedImage): BufferedImage = {
+        val w = img.getWidth
+        val h = img.getHeight
+
+        val out = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB)
+
+        for (x <- 0 until w)
+            for (y <- 0 until h){
+                val color = img.getRGB(x, y) & 0xffffff
+                var red = (color & 0xff0000) / 65536
+                var green = (color & 0xff00) / 256
+                var blue = (color & 0xff)
+                red = (round(b*red.toDouble)).toInt
+                if (red >= 0xff)
+                    red = 0xff
+                green = (round(b*green.toDouble)).toInt
+                if (green >= 0xff)
+                    green = 0xff
+                blue = (round(b*blue.toDouble)).toInt
+                if (blue >= 0xff)
+                    blue = 0xff
+                out.setRGB(x, y, (red << 16) + (green << 8) + blue)
+            }
+        out
     }
 
     def test() = {
@@ -74,10 +98,12 @@ class EditImage(val image: Image) {
     
     //val photo2 = myMethod(photo1) 
     val photo3 = rotate(photo1)
+    //val photo4 = brightness(2, photo1)
 
     // sacuvaj
     //ImageIO.write(photo2, "jpg", new File("mirror.jpg"))
     ImageIO.write(photo3, "jpg", new File("rotate.jpg"))
+    //ImageIO.write(photo4, "jpg", new File("brightness.jpg"))
     }
 
     /*
