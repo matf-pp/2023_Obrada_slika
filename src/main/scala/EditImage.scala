3c130089e97384fc48fc8d833b54b099e9bb868c
Irina Šević <127663394+irinasevic@users.noreply.github.com>
2023-04-28 15:30:43 +0200

    Add files via upload
    Unapređeni Main i klasa

---
+++ src/main/scala/EditImage.scala
@@ -0,0 +1,13 @@
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.image.Image
+import scalafx.scene.image
+import scalafx.scene.image.ImageView
+import scalafx.scene.layout.BorderPane
+
+class EditImage(val image: Image) {
+  def myMethod(): Unit = {
+    println("Button clicked!")
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,7 @@ import scalafx.scene.paint.Color
 import scalafx.scene.paint.Color._
 import scalafx.scene.paint._
 import scalafx.scene.text.Text
+import scalafx.scene.control.{Button, TextField}
 
 import java.io.FileInputStream
 import scalafx.scene.image.{Image, ImageView, PixelReader}
@@ -20,9 +21,10 @@ object Main extends JFXApp3 :
     // url slika
     val url = "https://images.squarespace-cdn.com/content/v1/54e7a1a6e4b08db9da801ded/1600464166638-5C0HPLJ9HT1TZBEHWVJR/Screen+Shot+2020-09-17+at+2.44.15+PM.png"
     val image2 = Image(url)
+    val editImage = new EditImage(image2)
     val imageView2 = new ImageView(image2)
     imageView2.fitWidth = 200
-    imageView2.fitHeight = 200 * image.height.value / image.width.value
+    imageView2.fitHeight = 200 * image2.height.value / image2.width.value
 
     // rgb (0,0) piksela
     val pixelReader = image.pixelReader.get
@@ -33,42 +35,80 @@ object Main extends JFXApp3 :
     println(s"r:$red, g:$green, b:$blue")
 
 
-    stage = new JFXApp3.PrimaryStage {
-      title = "Aplikacija"
-      scene = new Scene {
-        fill = Color.rgb(38, 38, 38)
+    stage = new JFXApp3.PrimaryStage:
+      width = 800
+      height = 500
+      title = "Obrada slika"
 
-        val hBox1 = new HBox {
-            padding = Insets(50, 80, 50, 80)
+
+      val buttonLoadFromLibrary = new Button {
+        text = "Load from library"
+        layoutX = 50
+        layoutY = 100
+        
+        style = "-fx-font: normal bold 10pt sans-serif; -fx-text-fill: gray; -fx-background-color: null; -fx-border-color: null;"
+        onMouseEntered = _ => style = "-fx-font: normal bold 10pt sans-serif; -fx-text-fill: black; -fx-background-color: null; -fx-border-color: null;"
+        onMouseExited  = _ => style = "-fx-font: normal bold 10pt sans-serif; -fx-text-fill: gray; -fx-background-color: null; -fx-border-color: null;"
+        
+        onAction = _ => {
+          val url = textField.text()
+          val image2 = Image(new FileInputStream("/home/irina/Desktop/image.jpeg"))
+          imageView2.image = image2
+          imageView2.fitWidth = 200
+          imageView2.fitHeight = 200 * image2.height.value / image2.width.value
+          editImage.myMethod()
+        }
+      }
+
+      val textField=new TextField {
+          layoutX = 50
+          layoutY = 50
+          prefWidth = 50
+        }
+
+      val buttonLoadURL = new Button {
+        text = "Load using URL:"
+        layoutX = 50
+        layoutY = 100
+        
+        style = "-fx-font: normal bold 10pt sans-serif; -fx-text-fill: gray; -fx-background-color: null; -fx-border-color: null;"
+        onMouseEntered = _ => style = "-fx-font: normal bold 10pt sans-serif; -fx-text-fill: black; -fx-background-color: null; -fx-border-color: null;"
+        onMouseExited  = _ => style = "-fx-font: normal bold 10pt sans-serif; -fx-text-fill: gray; -fx-background-color: null; -fx-border-color: null;"
+        
+        onAction = _ => {
+          val url = textField.text()
+          val image2 = Image(url)
+          // println(url)
+          // npr. https://img.freepik.com/free-photo/red-white-cat-i-white-studio_155003-13189.jpg?w=360
+          imageView2.image = image2
+          imageView2.fitWidth = 200
+          imageView2.fitHeight = 200 * image2.height.value / image2.width.value
+        }
+      }
+
+      val vBoxLeft = new VBox {
+            style = "-fx-background-color: #FFFFFF;"
+            padding = Insets(10, 10, 10, 10)
+            prefWidth = 200
+
             children = Seq(
-              new Text {
-                text = "Scala"
-                style = "-fx-font: normal bold 60pt sans-serif"
-                fill = new LinearGradient(
-                  endX = 0,
-                  stops = Stops(Red, DarkRed))
-              },
-              new Text {
-                text = "FX"
-                style = "-fx-font: italic bold 60pt sans-serif"
-                fill = new LinearGradient(
-                  endX = 0,
-                  stops = Stops(White, DarkGray)
-                )
-              }
+              buttonLoadFromLibrary,
+              buttonLoadURL,
+              textField
             )
         }
 
-        content = new VBox {
-            padding = Insets(50, 80, 50, 80)
+        
+      scene = new Scene :
+        fill = Color.rgb(210, 210, 210)
+
+        content = new HBox {
+            padding = Insets(10, 10, 10, 10)
             children = Seq(
-              hBox1,
-              imageView,
+              vBoxLeft,
               imageView2
           )
         }
-      }
-    }
 
 /*
 @main def hello: Unit =
